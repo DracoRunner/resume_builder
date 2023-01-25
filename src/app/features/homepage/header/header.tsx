d53bfbd747983e896ab89687f5d0dc5ac710a4f7
dracorunner <mahibaghel@live.co.uk>
2023-01-25 16:27:45 +0800

    Updated header with responsive navbar

--- src/app/features/homepage/header/header.tsx
+++ src/app/features/homepage/header/header.tsx
@@ -1,11 +1,19 @@
+import React from "react";
 import "./header.scss";
 export default function Header() {
+  const [isToggle, setToggle] = React.useState(false);
+
   return (
-    <section className="nav-lg">
+    <section className="nav">
       <div className="nav-brand">
         <a>Brand</a>
       </div>
-      <ul>
+      <div className="nav-btn" onClick={() => setToggle(!isToggle)}>
+        <a id="navbar-toggle" className={isToggle ? "active" : ""}>
+          <span></span>
+        </a>
+      </div>
+      <ul className={isToggle ? "active" : ""}>
         <li>Resume Builder</li>
         <li>Resume Examples</li>
         <li>Resume Templates</li>
